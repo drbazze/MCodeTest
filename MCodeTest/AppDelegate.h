b01ff2742fa291cfeba3d6f68d5091339447cfe5
Zumpf Tamás <tamas@Zumpf-MacBook-Pro.local>
2014-11-10 21:41:09 +0100

    Initial Commit

---
+++ MCodeTest/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  MCodeTest
+//
+//  Created by Zumpf Tamás on 2014. 11. 10..
+//  Copyright (c) 2014. Legion Services Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
